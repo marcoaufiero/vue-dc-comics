36ed827258505e3e6cb67b299a9397621fff26f9
marcoaufiero <marcoaufiero96@gmail.com>
2022-11-24 16:05:23 +0100

    ComicCard component and comics box added

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card">
+        <div class="img-box">
+            <img :src="comicDetails.thumb" alt="">
+        </div>
+        <h5>{{comicDetails.series.toUpperCase()}}</h5>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ComicCard',
+        props:{
+            comicDetails: Object
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .card{
+        width: calc(100% / 6);  
+        margin-top: 20px;
+        padding: 20px;
+        
+        .img-box{
+
+            width: 100%;
+            aspect-ratio: 1/1;
+            overflow: hidden;
+
+            img{
+                width: 100%;
+                
+            }
+        }
+       
+    }
+
+    
+
+    h5{
+        color: white;
+        margin-top: 20px;
+    }
+</style>
